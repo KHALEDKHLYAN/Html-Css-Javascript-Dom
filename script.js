64de77d4f1ea6a107afed347693f46b3ff7e8770
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-03-12 00:48:28 +0300

    js Function

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ document.getElementById("demo").innerHTML = "Date : " + Date();
 
 // In JavaScript, document.write() can be used to write directly to the HTML output stream:
 document.write(Date());
+
+const validateForm = () => {
+    let x = document.forms[ "myForm" ][ "fname" ].value;
+    if (x == "") {
+        alert("Name must be filled out");
+        return false;
+    }
+}
