68a7195c470b1e112f15ce9cbf1dfb93cbb3cfbd
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-03-12 00:31:13 +0300

    additional js

---
+++ script.js
@@ -0,0 +1,5 @@
+
+document.getElementById("demo").innerHTML = "Date : " + Date();
+
+// In JavaScript, document.write() can be used to write directly to the HTML output stream:
+document.write(Date());
