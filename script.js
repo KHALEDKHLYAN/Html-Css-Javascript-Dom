5967c45314f612970a596558c2a232cc621c69c8
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-03-12 23:32:47 +0300

    addition and addEventsListner in js

--- script.js
+++ script.js
@@ -30,3 +30,24 @@ const myMove = () => {
         }
     }
 }
+
+    let p1 = 5;
+    let p2 = 7;
+    document.getElementById("myBtn").addEventListener("click", function() {
+        myFunction(p1, p2);
+    });
+
+    function myFunction(a, b) {
+        document.getElementById("demo").innerHTML = a * b;
+    }
+    const num1 = document.getElementById('num1');
+    const num2 = document.getElementById('num2');
+    const num3 = document.getElementById('num3');
+
+    num1.addEventListener('input', updateSum);
+    num2.addEventListener('input', updateSum);
+
+    function updateSum() {
+      const sum = Number(num1.value) + Number(num2.value);
+      num3.value = sum;
+    }
