63782fcb041f06dfd2e28dc5ce4697d94c5d0e77
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-03-12 08:51:00 +0300

    Animating

--- script.js
+++ script.js
@@ -11,3 +11,22 @@ const validateForm = () => {
         return false;
     }
 }
+
+
+// animating with Javascript
+const myMove = () => {
+    let id = null;
+    const elem = document.getElementById("animate");
+    let pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 5);
+    function frame() {
+        if (pos == 350) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.top = pos + "px";
+            elem.style.left = pos + "px";
+        }
+    }
+}
